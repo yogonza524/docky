4f64b5d8119fc5126f763f90fa22d64bab3080f3
Gonza <yogonza524@gmail.com>
2016-09-03 21:32:54 -0300

    Realizando cambios

--- src/main/java/com/core/beans/IndexBean.java
+++ src/main/java/com/core/beans/IndexBean.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.UnaryOperator;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;

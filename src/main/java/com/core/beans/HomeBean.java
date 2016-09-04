926a0920404296fb8237b3c62738de6e2aed5e99
Gonza <yogonza524@gmail.com>
2016-09-03 22:51:55 -0300

    Cambios generales

---
+++ src/main/java/com/core/beans/HomeBean.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.core.beans;
+
+import com.core.controller.Kimera;
+import com.core.entities.Project;
+import com.core.util.HibernateUtil;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author gonza
+ */
+@ManagedBean(name="home")
+@ViewScoped
+public class HomeBean {
+    
+    private Kimera k;
+    private List<Project> projects;
+    private Project newProject;
+
+    public Project getNewProject() {
+        return newProject;
+    }
+
+    public void setNewProject(Project newProject) {
+        this.newProject = newProject;
+    }
+    
+    @PostConstruct
+    public void init(){
+        k = new Kimera(HibernateUtil.getSessionFactory());
+        projects = k.all(Project.class);
+        newProject = new Project();
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+}
